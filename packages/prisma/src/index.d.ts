8f0d3f83be3cfc727730502e609889033fa4c777
subhdeep8384 <113197481+subhdeep8384@users.noreply.github.com>
2025-11-17 14:39:49 +0530

    package package json corrected from ts to js

--- packages/prisma/src/index.d.ts
+++ packages/prisma/src/index.d.ts
@@ -1,3 +1,4 @@
-declare const client: import("../generated/prisma/internal/class").PrismaClient<never, import("../generated/prisma/internal/prismaNamespace").GlobalOmitConfig | undefined, import("@prisma/client/runtime/library").DefaultArgs>;
+import { PrismaClient } from "@prisma/client";
+declare const client: PrismaClient<import("@prisma/client").Prisma.PrismaClientOptions, never, import("@prisma/client/runtime/library").DefaultArgs>;
 export default client;
 //# sourceMappingURL=index.d.ts.map
